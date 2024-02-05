e0d60dca02a2ac0dd05288de38f0653efb430bdb
Zinath22 <Zinath22@users.noreply.github.com>
2024-02-05 10:53:57 +0600

    boys card section add

--- src/Components/BoysCollection/BoysCollection.jsx
+++ src/Components/BoysCollection/BoysCollection.jsx
@@ -1,3 +1,4 @@
+import BoysCollectionCard from "./BoysCollectionCard";
 
 
 const BoysCollection = () => {
@@ -6,11 +7,13 @@ const BoysCollection = () => {
            <div className="flex-1">
                 <h1 className="text-2xl font-semibold">For Girls</h1>
                 <hr />
-                hhh
+                <div className="mt-5">
+                <BoysCollectionCard></BoysCollectionCard>
+                </div>
             </div>
 
             <div className="flex-1">
-                <img src="https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZmFzaGlvbiUyMGdpcmx8ZW58MHx8MHx8fDA%3D" alt="" />
+                <img src="https://media.istockphoto.com/id/155445946/photo/teenage-boy-shopping.jpg?s=612x612&w=0&k=20&c=YQQa3HHvuank-cBHUPEH-y7K8c4ltNSRjGPVwzLeexc=" alt="" />
             </div>
 
         </div>
