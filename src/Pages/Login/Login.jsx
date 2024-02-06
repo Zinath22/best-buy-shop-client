9a61523446db706be6885aee072a2b10e1e3e87c
Zinath22 <Zinath22@users.noreply.github.com>
2024-02-06 23:34:47 +0600

    validation add

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,14 @@
 
 
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+
     return (
         <div>
         
@@ -10,24 +18,25 @@ const Login = () => {
  
   <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 lg:text-left">
    <h1 className="text-center my-5 text-3xl font-semibold text-yellow-500">Please Login</h1>
-   <form className="card-body">
+   <form onSubmit={handleLogin} className="card-body">
      <div className="form-control">
        <label className="label">
          <span className="label-text">Email</span>
        </label>
-       <input type="email" placeholder="email" className="input input-bordered" required />
+       <input type="email" name="email" placeholder="email" className="input input-bordered" required />
      </div>
      <div className="form-control">
        <label className="label">
          <span className="label-text">Password</span>
        </label>
-       <input type="password" placeholder="password" className="input input-bordered" required />
+       <input type="password" name="password" placeholder="password" className="input input-bordered" required />
        <label className="label">
          <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
        </label>
      </div>
      <div className="form-control mt-6">
        <button className="btn btn-primary">Login</button>
+       {/* <input className="btn btn-primary" type="Submit" value={"login"} /> */}
      </div>
    </form>
  </div>
