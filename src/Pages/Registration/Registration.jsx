faa370096a73a15028c850fcf7022f4856469248
Zinath22 <Zinath22@users.noreply.github.com>
2024-02-14 22:20:55 +0600

    handle reg add

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,6 +1,15 @@
 
 
 const Registration = () => {
+
+  const handleLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+}
+
     return (
         <div>
             Please registration
